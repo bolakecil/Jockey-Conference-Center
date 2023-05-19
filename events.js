3281286739f69911cde5ef72e6e1b4da088e9b88
bolakecil <jeslynn30@gmail.com>
2023-05-19 09:54:58 +0700

    Events page close to done!
    Lack a few edits on input in newsletter, otherwise, all pages are connected and validation is running smoothly in JS!

---
+++ events.js
@@ -0,0 +1,29 @@
+document.getElementById("button").onclick = function () {
+    let email = document.getElementById("email").value;
+    if (email == "") {
+        alert("Email should not be empty!");
+        return false;
+    } else if (email.split("@").length > 2 || email.split("@").length == 1) {
+        alert("Email should contain '@'!");
+        return false;
+    } else if (!email.split("@")[1].includes(".com")) {
+        alert("Email should contain '.com' after '@'!");
+        return false;
+    }
+};
+
+document.getElementById("week1").onclick = function(){
+    location.href = "events1.html";
+}
+
+document.getElementById("week2").onclick = function(){
+    location.href = "events2.html";
+}
+
+document.getElementById("week3").onclick = function(){
+    location.href = "events3.html";
+}
+
+document.getElementById("week4").onclick = function(){
+    location.href = "events4.html";
+}
